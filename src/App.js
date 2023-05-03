a5773729d95af2b9d3bbc7538ff2962aa403826f
valentina vittoria <valeviit@icloud.com>
2023-05-03 18:05:28 +0200

    create Posts component & fetch data in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from 'axios';
+import Posts from "./components/Posts";
 
 function App() {
-  return (
-    <>
-      <h1>Hola Mundo !</h1>
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerpage] = useState(12);
 
-    </>
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false)
+    }
+    fetchPosts();
+  }, []);
+
+  
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog !</h1>
+      <Posts posts={posts} loading={loading} />
+
+    </div>
   );
 }
 
