54f33400b1f7e7ca49876c59dae12bc808723553
valentina vittoria <valeviit@icloud.com>
2023-05-04 18:33:27 +0200

    pagination

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import axios from 'axios';
 import Posts from "./components/Posts";
+import  Pagination from "./components/Pagination";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -19,13 +20,20 @@ function App() {
     }
     fetchPosts();
   }, []);
+//Get current Post
+  const indexLastPost = currentPage * postsPerPage;
+  const indexOfFirsPost = indexLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirsPost, indexLastPost);
 
+  //ChangePage
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog !</h1>
-      <Posts posts={posts} loading={loading} />
-
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
     </div>
   );
 }
